d5d8cd40234af98796f1cd9e329a071e80551822
Jascinth Rhema <jascinthrhema@gmail.com>
2026-05-24 20:53:34 +0530

    fixed render issue

--- server.js
+++ server.js
@@ -1,27 +1,46 @@
 const express = require("express");
+const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
+// Public folder serve panna
 app.use(express.static(path.join(__dirname, "public")));
 
-app.post("/submit", (req, res) => {
-  const complaint = req.body;
+// Homepage route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
+// Complaints file
+const complaintsFile = path.join(__dirname, "complaints.json");
+
+// Get all complaints
+app.get("/complaints", (req, res) => {
+  if (fs.existsSync(complaintsFile)) {
+    const data = fs.readFileSync(complaintsFile);
+    res.json(JSON.parse(data));
+  } else {
+    res.json([]);
+  }
+});
+
+// Add complaint
+app.post("/complaints", (req, res) => {
   let complaints = [];
 
-  if (fs.existsSync("complaints.json")) {
-    const data = fs.readFileSync("complaints.json");
-    complaints = JSON.parse(data);
+  if (fs.existsSync(complaintsFile)) {
+    complaints = JSON.parse(fs.readFileSync(complaintsFile));
   }
 
-  complaints.push(complaint);
+  complaints.push(req.body);
 
   fs.writeFileSync(
-    "complaints.json",
+    complaintsFile,
     JSON.stringify(complaints, null, 2)
   );
 
@@ -30,15 +49,7 @@ app.post("/submit", (req, res) => {
   });
 });
 
-app.get("/complaints", (req, res) => {
-  if (fs.existsSync("complaints.json")) {
-    const data = fs.readFileSync("complaints.json");
-    res.json(JSON.parse(data));
-  } else {
-    res.json([]);
-  }
-});
-
+// Server start
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
